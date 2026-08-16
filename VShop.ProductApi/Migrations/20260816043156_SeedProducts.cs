0bd8f25d748627bf6b4f19b8e450a6fc946cbe42
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-08-16 17:38:36 -0300

    feat: Criação do modelo de entidades e conexão ao banco

---
+++ VShop.ProductApi/Migrations/20260816043156_SeedProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name, Price, Description, Stock, ImageURL, CategoryId) " +
+                    "VALUES ('Caderno', 7.55, 'Caderno', 10, 'caderno1.jpg', 1)");
+
+            mb.Sql("INSERT INTO Products(Name, Price, Description, Stock, ImageURL, CategoryId) " +
+                   "VALUES ('Lápis', 3.55, 'Lápis Preto', 20, 'lapis1.jpg', 1)");
+
+            mb.Sql("INSERT INTO Products(Name, Price, Description, Stock, ImageURL, CategoryId) " +
+                   "VALUES ('Clips', 5.34, 'Clips para papel', 50, 'clips1.jpg', 2)");  
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE from Products");
+        }
+    }
+}
