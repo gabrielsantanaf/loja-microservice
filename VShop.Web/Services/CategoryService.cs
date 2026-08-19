6de67bc792c9624cfc0da1ecbde2aeaaf72c1072
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-08-19 01:37:25 -0300

    feat(Vshop.Web):Criação da funcionalidade de criação de produto juntamente com a View

---
+++ VShop.Web/Services/CategoryService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
+
+namespace VShop.Web.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly JsonSerializerOptions _options;
+        private const string apiEndpoint = "/api/categories/";
+
+        public CategoryService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<IEnumerable<CategoryViewModel>> GetAllCategories()
+        {
+            var client = _clientFactory.CreateClient("ProductApi");
+
+            using var response = await client.GetAsync(apiEndpoint);
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var apiResponse = await response.Content.ReadAsStreamAsync();
+            var categoryVM = await JsonSerializer.DeserializeAsync<IEnumerable<CategoryViewModel>>(apiResponse, _options);
+
+            return categoryVM;
+        }
+    }
+}
